09dd84d6a904ef0bb8060045bc2c4e29f8a7b550
ali99944 <alitarek99944@gmail.com>
2025-12-04 05:57:12 +0200

    update app constants

--- src/constants/app_constants.ts
+++ src/constants/app_constants.ts
@@ -1,5 +1,5 @@
 // export const api_url = "http://localhost:8000/api";
 // export const storage_url = "http://localhost:8000/storage";
 
-export const api_url = "https://sansegal-api.shopaiklopaik.com/public/api";
-export const storage_url = "https://sansegal-api.shopaiklopaik.com/public";
+export const api_url = "https://sapi.shopaiklopaik.com/api";
+export const storage_url = "https://sapi.shopaiklopaik.com/storage";
